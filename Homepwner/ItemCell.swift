0f6865690e530afa4ffe7ef8c4bb71c4c6d31d65
Cara Weggler <cweggler@gmail.com>
2019-02-26 00:00:52 -0600

    Added code to adjust for the changes in font size for accessibility

--- Homepwner/ItemCell.swift
+++ Homepwner/ItemCell.swift
@@ -14,6 +14,14 @@ class ItemCell: UITableViewCell {
     @IBOutlet var serialNumberLabel: UILabel!
     @IBOutlet var valueLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
     
     
 }
