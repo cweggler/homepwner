45cd83eab27ea970a994d62b45d8eaa133caa56e
Cara Weggler <cweggler@gmail.com>
2019-02-25 23:28:13 -0600

    Created ItemCell class and made layout on storyboard

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Cara on 2/25/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    
+    
+}
