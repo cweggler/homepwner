508dffabb03f45af074014c13ad6bfe5aacc2071
Cara Weggler <cweggler@gmail.com>
2019-03-26 06:56:26 -0500

    removed the header view

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -109,13 +109,6 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Get the height of the status bar
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
-        
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 65
         
